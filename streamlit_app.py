38e2a0c72056acaaf9d8bcffa4668cd4dcd130a8
Madhukotte <Madhukotte017@gmail.com>
2024-06-11 19:24:40 +0530

    Update streamlit_app.py
    Updating the latest values for SEARCH_ON

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,8 +33,9 @@ st.write(
 #COl statmenet
 #from snowflake.snowpark.functions import col
 #session = get_active_session()
-#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 #Add a Multiset
 name_on_order = st.text_input('Name on the Smoothie:')
