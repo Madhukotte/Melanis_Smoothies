f891fbad56d91ff34fa1ecae0ea2bcffa3993a16
Madhukotte <Madhukotte017@gmail.com>
2024-06-11 18:42:49 +0530

    Update streamlit_app.py
    Let's Expose the JSON Data Inside the Response Object

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,9 +74,9 @@ if time_to_insert:
 #New section to display fruity vice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
-
-
+#st.text(fruityvice_response.json())
+#Let's Put the JSON into a Dataframe
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
 
