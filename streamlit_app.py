4d796326053f4a22f32f3843484917a6b1a0b27c
Madhukotte <Madhukotte017@gmail.com>
2024-06-25 08:43:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
 # Write directly to the app
@@ -32,7 +32,7 @@ st.write(
 
 #COl statmenet
 #from snowflake.snowpark.functions import col
-session = get_active_session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
