bf53563db8b52cbaa081775c189d40f1e76c1faf
Madhukotte <Madhukotte017@gmail.com>
2024-06-25 09:28:07 +0530

    Update streamlit_app.py
    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,35 @@
-# Import python packages
-import streamlit as st
+#Import python packages
+import streamlit as st 
 #from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col
-import requests
-# Write directly to the app
-st.title("Customize Your Smoothie! :balloon:")
-st.write(
-    """Choose the fruits you want in your custome Smoothie!
-    """
-)
-
+  from snowflake.snowpark.functions import col 
+ import requests 
+#Write directly to the app
+  st.
+title ("Customize Your Smoothie! :balloon:") 
+  st.
+write (
+"" "Choose the fruits you want in your custome Smoothie!
+    " "" 
+) 
+ 
 #Adding Interactive Elements
 #import streamlit as st
 
-# option =st.selectbox(
-#   'How would you like to be contacted?',
-#    ('Email','Home Phone','Mobile phone')
-# )
+#option =st.selectbox(
+#'How would you like to be contacted?',
+#('Email','Home Phone','Mobile phone')
+#)
 
-# st.write('You selected:',option)
+#st.write('You selected:',option)
 
 #Let's ask about fruits instead of contact methods.
 
-# option =st.selectbox(
-#    'What is your fevorite fruit?',
-#    ('Banana','Strawberries','Peaches')
-# )
+#option =st.selectbox(
+#'What is your fevorite fruit?',
+#('Banana','Strawberries','Peaches')
+#)
 
-# st.write('You selected:',option)
+#st.write('You selected:',option)
 
 
 #COl statmenet
@@ -38,52 +40,76 @@ st.write(
 #st.stop()
 
 #Add a Multiset
-name_on_order = st.text_input('Name on the Smoothie:')
-st.write('The name on the Smoothie will be:', name_on_order)
-
-cnx = st.connection("snowflake")
-session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+name_on_order = st.text_input ('Name on the Smoothie:') 
+  st.write ('The name on the Smoothie will be:', name_on_order) 
+  
+cnx = st.connection ("snowflake") 
+  session = cnx.session () 
+  my_dataframe =
+  session.table ("smoothies.public.fruit_options").
+select (col ('FRUIT_NAME')) 
 #st.dataframe(data=my_dataframe, use_container_width=True)
-
-
-
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+  
+ 
+ 
+my_dataframe =
+  session.table ("smoothies.public.fruit_options").select (col ('FRUIT_NAME'),
+														   col ('SEARCH_ON'))
+  
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
-
-# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
-pd_df=my_dataframe.to_pandas()
+  
+#Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+  pd_df = my_dataframe.to_pandas () 
 #st.dataframe(pd_df)
 #st.stop()
-
-ingredients_list = st.multiselect(
-    'Choose upto 5  ingredients:'
-    ,my_dataframe
-    ,max_selections=5
-)
-if ingredients_list:
-   ingredients_string=''
-    
-for fruit_chosen in ingredients_list:
-    ingredients_string += fruit_chosen + ''
-
-    search_on=pd_df.loc(pd_df('FRUIT_NAME') == fruit_chosen, 'SEARCH_ON').iloc[0]
-    st.write('The search value for', fruit_chosen,'is',search_on,'.')
-    
-    st.subheader(fruit_chosen + 'Nutrition Information')
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
-    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+  
+ingredients_list =
+  st.multiselect (
+'Choose upto 5  ingredients:' 
+, my_dataframe 
+,
+				  max_selections = 5 
+) 
+ if ingredients_list
+:
+ingredients_string = '' 
+ 
+for fruit_chosen
+  in ingredients_list:
+ingredients_string += fruit_chosen + '' 
+	  
+search_on =
+	  pd_df.loc (pd_df ('FRUIT_NAME') == fruit_chosen,
+				 'SEARCH_ON').iloc[0] 
+ st.write ('The search value for',
+												  fruit_chosen, 'is',
+												  search_on,
+												  '.') 
+ 
+st.
+	  subheader (fruit_chosen +
+				 'Nutrition Information') 
+ fruityvice_response =
+	  requests.get ("https://fruityvice.com/api/fruit/" +
+					fruit_chosen) 
+ fv_df = st.dataframe (data =
+														  fruityvice_response.
+														  json (),
+														  use_container_width
+														  = True) 
 #st.write(ingredients_string)
-
-
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-
-st.write(my_insert_stmt)
-# st.stop()
-
-time_to_insert = st.button('Submit Order')
-if time_to_insert:
-   session.sql(my_insert_stmt).collect()
-   st.success('Your Smoothie is ordered!',icon="✅")
+	  
+ 
+my_insert_stmt =
+	  "" " insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('" "" + ingredients_string + "" "','" "" + name_on_order + "" "')" "" 
+ 
+st.write (my_insert_stmt) 
+#st.stop()
+	  
+time_to_insert = st.button ('Submit Order') 
+ if time_to_insert
+	:
+session.sql (my_insert_stmt).collect () 
+		st.success ('Your Smoothie is ordered!', icon = "b") 
