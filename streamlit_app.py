4bc7540070db208813a5fbbf5a4bb9a70afaf2db
Madhukotte <Madhukotte017@gmail.com>
2024-06-25 14:36:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Title and introduction
 st.title("Customize Your Smoothie! :balloon:")
